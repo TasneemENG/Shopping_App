ab00fa6d9c075029da6f7c70521fefc773f8f7a7
Tasneem Khaled <tasneemshaheen2004@gmail.com>
2024-07-27 18:52:38 +0300

    tasneem5

--- lib/features/Splash/presentation/views/screens/onboarding_view.dart
+++ lib/features/Splash/presentation/views/screens/onboarding_view.dart
@@ -1,10 +1,71 @@
 import 'package:animal_app/features/Splash/presentation/views/widgets/onboading_body.dart';
 import 'package:flutter/material.dart';
-class onboarding_view extends StatelessWidget {
+import 'package:liquid_swipe/liquid_swipe.dart';
+class onboarding_view extends StatefulWidget {
   const onboarding_view({super.key});
 
   @override
+  State<onboarding_view> createState() => _onboarding_viewState();
+}
+
+class _onboarding_viewState extends State<onboarding_view> with SingleTickerProviderStateMixin {
+
+
+
+  @override
   Widget build(BuildContext context) {
-    return const onboarding();
+    return Scaffold(
+      body: LiquidSwipe(
+        pages: [
+          // First page with a gradient background and gradient text
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.orange, Colors.yellow,Colors.orange],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Center(
+              child: ShaderMask(
+                shaderCallback: (bounds) => LinearGradient(
+                  colors: [Colors.white60, Colors.white],
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                ).createShader(bounds),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Welcome To ',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                        //color: Colors.white, // The color here is used as a fallback
+                      ),
+                    ),
+                    Text(
+                      'Shopping APP',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                        //olor: Colors.white, // The color here is used as a fallback
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          // Onboarding view as the second page
+          onboarding(),
+        ],
+        enableLoop: false, // Disable looping if you want to go directly to onboarding_view
+        fullTransitionValue: 300, // Customize this value as needed
+      ),
+    );
   }
 }
